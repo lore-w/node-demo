bc38e409b68dd9b39f216e447d52ff138417fa24
lore-w <metro-cpu@hotmail.com>
2015-08-24 18:12:39 +0800

    upload text

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,20 +1,30 @@
 var exec = require('child_process').exec;
+var querystring = require('querystring');
 
-function start (response) {
+function start (response, postData) {
 	console.log('request handler "start" was called');
 
-	//非阻塞例子
-	exec("ls -lah",function (err, stdout, stderr) {
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write(stdout);
-		response.end();
-	});
+	var body = '<html>' +
+	'<head>' +
+	'<meta http-equiv="Content-Type" content="text/html" charset="UTF-8">' +
+	'</head>' +
+	'<body>' +
+	'<form action="/upload" method="post">' +
+	'<textarea name="text" rows="20" cols="50"></textarea><br />' +
+	'<input type="submit" value="submit" />' +
+	'</form>' +
+	'</body>' +
+	'</html>';
+
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write(body);
+	response.end();
 }
 
-function upload (response) {
+function upload (response, postData) {
 	console.log('request handler "upload" was called');
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello Upload");
+	response.write("You's sent " + querystring.parse(postData).text);
 	response.end();
 }
 
